40dff30134e80d6e0646327006825d9c58429d60
markicevic <markicevic@markicevic-K53E.(none)>
2015-05-11 15:10:05 +0200

    test

--- application/modules/default/controllers/UserController.php
+++ application/modules/default/controllers/UserController.php
@@ -180,6 +180,8 @@ class UserController extends Zend_Controller_Action {
 
 			if($result->success == true){
 				$user = $this->my_service_users->get($user_id);
+				
+				$this->user_auth->clearIdentity();
 				$this->auth_storage->write($this->user);
 				$this->_redirect('user/profile');
 			}else{
