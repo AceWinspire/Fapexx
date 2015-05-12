2a620317b5415b6d3cb29bcf63f5927ebf9b9a86
Milan Marjanovic <milan.marjanovic@golive.rs>
2015-05-12 09:32:05 +0200

    fix

--- application/modules/default/controllers/ContentController.php
+++ application/modules/default/controllers/ContentController.php
@@ -11,6 +11,10 @@
  */
 class ContentController extends Zend_Controller_Action {
 
+	private $user;
+	private $user_auth;
+	private $auth_storage;
+
 	private $my_service_contents;
 	private $my_service_log;
 	private $my_service_app;
@@ -19,6 +23,10 @@ class ContentController extends Zend_Controller_Action {
 	private $memcache_options;
 
 	public function init(){
+		$this->user_auth 	= Zend_Auth::getInstance();
+		$this->auth_storage = $this->user_auth->getStorage();
+		$this->user = $this->user_auth->getIdentity();
+
 		$this->my_service_contents  = new My_Service_Contents();
 		$this->my_service_log  		= new My_Service_Log();
 		$this->my_service_app  		= new My_Service_App();
@@ -35,29 +43,19 @@ class ContentController extends Zend_Controller_Action {
 
 	public function videoAction(){
 		$ip_address = Zend_Registry::get('remote_ip');
-		
-		try {
-				$memcache = new Memcache;
-				$memcache->connect($this->memcache_options['host'], $this->memcache_options['port']);
-			} catch (Exception $e) {
-				throw new Exception('Error conecting memcache server ' . $e->getMessage());
-			}		
 
-		$code_try_count = $memcache->get($this->memcache_options['prefix'] . $ip_address);
-
-		$memcache->set($this->memcache_options['prefix'] . $ip_address, (($code_try_count) ? ++$code_try_count : 1), false);
-		
-		if ($code_try_count > $this->app_settings->show_subscribe_prompt_value) {
-			$this->_redirect('/index/packages/');
-		}
-		
 		$this->view->cat_id = $this->_getParam('cat_id', 0);
 		$video_id = $this->_getParam('id');
-		
-		$this->view->existing = $this->my_service_contents->getContent($video_id);
-		if($this->view->cat_id == 0){     //from index-page
+
+		$video  =  $this->my_service_contents->getContent($video_id);
+		/*if($this->user && $this->user->charged == true && $video->is_premium){
+			$video->url = $video->preview_url;
+		}*/
+		$this->view->existing = $video;
+
+		if($this->view->cat_id == 0){
 			$this->view->videos = $this->my_service_contents->getFeaturedContent();
-		}else{                           //from category page
+		}else{
 			$this->view->videos = $this->my_service_contents->getVideos($this->view->cat_id);
 		}
 		$this->my_service_log->contentViewed($this->view->existing->id, $ip_address);
@@ -66,7 +64,7 @@ class ContentController extends Zend_Controller_Action {
 	public function streamAction(){
 		$ip_address = Zend_Registry::get('remote_ip');
 		
-		try {
+		/*try {
 				$memcache = new Memcache;
 				$memcache->connect($this->memcache_options['host'], $this->memcache_options['port']);
 			} catch (Exception $e) {
@@ -79,7 +77,7 @@ class ContentController extends Zend_Controller_Action {
 
 		if ($code_try_count > $this->app_settings->show_subscribe_prompt_value) {
 			$this->_redirect('/index/packages/');
-		}
+		}*/
 		
 		$video_id = $this->_getParam('id');
 		$this->view->existing = $this->my_service_contents->getContent($video_id);
