cdcb5c42459675cc9b687a9aef14352fd1c32914
Milan Marjanovic <milan.marjanovic@golive.rs>
2015-05-19 12:21:26 +0200

    fix

--- library/My/Plugin/Front.php
+++ library/My/Plugin/Front.php
@@ -29,8 +29,8 @@ class My_Plugin_Front extends Zend_Controller_Plugin_Abstract {
             $request->setActionName('desktop');
         }else if($device->isMobile() && !$no_android){
             $request->setControllerName('index');
-            $request->setActionName('android');
-            //$request->setActionName('index');
+            //$request->setActionName('android');
+            $request->setActionName('index');
         }
     }
 }
